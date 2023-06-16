bb3892cb3535ef6de92a12923bf3b13d7c093db7
Berthain <rauljrom@gmail.com>
2023-06-16 00:07:41 -0300

    creating model struct

---
+++ config/variables.py
@@ -0,0 +1,12 @@
+import os
+
+
+WEBHOOK = 'https://hhvvd.webhook.office.com/webhookb2/'
+CHANNEL = 'd0c11d9b-31c5-46b7-a07d-d3e9a485f9e9@d0f183a4-42a5-4f41-a0ea-a8bd15ee8c84/IncomingWebhook/a67457d3ef12433aaf188bcacf22eea5/bdf246da-fd03-4240-9703-7b58e6f63ae1'
+
+STOCK_CSV = '\\stock.csv'
+
+LOCAL_DIR = os.getcwd()
+
+OUTPUT = LOCAL_DIR+STOCK_CSV
+
